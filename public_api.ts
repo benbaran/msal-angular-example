323202a8231e0b32b541e37af863963ae4fc8b00
Ben Baran <ben.baran@gmail.com>
2018-01-25 13:18:54 -0600

    Added support for building npm module.

---
+++ public_api.ts
@@ -0,0 +1,6 @@
+export * from './src/app/msal/msal-config';
+export * from './src/app/msal/msal.service';
+export * from './src/app/msal/msal.guard';
+export * from './src/app/msal/msal-interceptor';
+
+
